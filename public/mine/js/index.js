5d0fefd4ba18f257d7257acbe184ee20c87aee4b
zhangting <270128687@qq.com>
2018-09-12 23:32:38 +0800

    finish first page then to category

---
+++ public/mine/js/index.js
@@ -0,0 +1,8 @@
+$(function(){
+    //var gallery = mui('.mui-slider');
+    mui('.mui-slider').slider({
+        interval:1000//自动轮播周期，若为0则不自动播放，默认为0；
+    });
+})/**
+ * Created by Administrator on 2018/9/12.
+ */
